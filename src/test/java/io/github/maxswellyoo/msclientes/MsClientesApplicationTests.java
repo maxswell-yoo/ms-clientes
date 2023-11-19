5ae83a8cb70a8d3fa3a47453fb2bd62b6a1b17b9
Maxswell Araújo <max.araujodev@gmail.com>
2023-11-19 01:51:59 -0500

    initial commit

---
+++ src/test/java/io/github/maxswellyoo/msclientes/MsClientesApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.maxswellyoo.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class MsClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
